f145704b5e9a44f6064be8183f3d61284fb73297
ginglis13 <ginglis05@gmail.com>
2020-01-22 23:06:55 +0000

    update skele

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3 
+
 import os
 import json
 from urllib.parse import urlencode
@@ -10,26 +12,28 @@ app = Flask(__name__)
 with open(".secret", "r") as f:
     bot_id = f.readlines()[0].strip()
 
+
 @app.route('/', methods=['POST'])
 def shortstop():
-	message = request.get_json()
+    message = request.get_json()
+    print(message)
 
-	# TODO: bot logic 
+    # TODO: bot logic 
 
-	return "ok", 200
+    return "ok", 200
 
 
 def reply(msg):
     """Reply to a message in the chat"""
-	url = 'https://api.groupme.com/v3/bots/post'
-	data = {
-		'bot_id'		: bot_id,
-		'text'			: msg
-	}
-	request = Request(url, urlencode(data).encode())
-	json = urlopen(request).read().decode()
+    url = 'https://api.groupme.com/v3/bots/post'
+    data = {
+        'bot_id'		: bot_id,
+        'text'			: msg
+    }
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
 
 
 def sender_is_bot(message):
     """Check if sender is bot to not reply to own msgs"""
-	return message['sender_type'] == "bot"
+    return message['sender_type'] == "bot"
