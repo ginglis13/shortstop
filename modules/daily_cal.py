2bc011e954a2449475a8c22c3f0d0101d1958681
Jack Fox <jfox13@nd.edu>
2020-01-23 00:23:40 -0800

    added google calendar module

---
+++ modules/daily_cal.py
@@ -0,0 +1,56 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+def daily_cal():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    tomorrow = datetime.datetime.utcnow() + datetime.timedelta(days=1) # 'Z' indicates UTC time
+    tomorrow = tomorrow.isoformat() + 'Z'
+
+    events_result = service.events().list(calendarId='nd.edu_sii4f2j1gio781dr6fkbuotjbc@group.calendar.google.com', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime', timeMax=tomorrow).execute()
+    events = events_result.get('items', [])
+    event_return = []
+
+    if not events:
+        event_return.append('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        event_return.append( '{} {}'.format(start,event['summary']) )
+    return event_return
+
+if __name__ == '__main__':
+    daily_cal()
