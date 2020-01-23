75b37fe18bea319e907d93ce5bf8eb93dd2f5b6a
ginglis13 <ginglis05@gmail.com>
2020-01-23 20:31:30 +0000

    party standardized

--- modules/party.py
+++ modules/party.py
@@ -1,9 +1,20 @@
+def party(message):
+    '''
+    party(message)
+        message: !party <content>
+        return:  Details for party
+    '''
+    # Usage
+    usage_message = "usage: !party <building> <room> <time>"
 
-def party(s):
-    if '!party' in s:
-        s = s.strip().split()
-        if len(s) > 3:
-            return "Imminent noise complaint at {} {}, {}".format(s[1], s[2], ' '.join(s[3:]))
-        else:
-            return "Usage: !party <building> <room> <time>"
+    # Correct format ought to be <build> <room> <time>
+    message_content = message.strip().split()[1:]
+
+    if len(message_content) == 1 and message_content[0] == "help":
+        return usage_message
+
+    if len(message_content) == 3:
+        return "Imminent noise complaint at {} {}, {}".format(message_content[0], message_content[1], message_content[2])
+    else:
+        return usage_message
     return None
