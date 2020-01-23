d4098d78bb7eacc4fc152d7bd1c2c033ef42921c
Jack Fox <jfox13@nd.edu>
2020-01-22 21:01:33 -0800

    fixed noise complaint bug

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def noise_complaint(msg):
     """If you're being too loud, register a noise complaint"""
     if msg == msg.upper():
         return "Noise complaint registered with Redwood room 202"
-    return none
+    return None
 
 
 def sign_in(msg):
