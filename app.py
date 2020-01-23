041e550fd865da58eea30b5af9edb5b874066a8d
Jack Fox <jfox13@nd.edu>
2020-01-22 21:38:35 -0800

    cleaned out dead code from app.py migration to modules

--- app.py
+++ app.py
@@ -20,16 +20,6 @@ from modules.bachelor import bachelor
 from modules.weather import weather_handler
 
 app = Flask(__name__)
-'''
-ZIPCODE = {
-    'redwood':  94062, # Redwood, CA
-    'nd':   46556, # Notre Dame, IN
-    'menlo': 94025, # Menlo, CA 
-    'att': 94301 # Palo Alto, CA
-}
-
-DEFAULT_ZIPCODE = 94025
-'''
 
 # get bot_id and api keys
 with open(".secret", "r") as f:
@@ -55,7 +45,6 @@ def shortstop():
         dierre_pic_handler(message, bot_id, application_id)
         if bachelor(message): reply(bachelor(message))
 
-
     return "ok", 200
 
 
@@ -74,45 +63,6 @@ def sender_is_bot(message):
     """Check if sender is bot to not reply to own msgs"""
     return message['sender_type'] == "bot"
 
-'''
-def weather_handler(s, OWM_APPID):
-    if '!weather' in s:
-        args = s.strip().split()
-        if len(args) > 1:
-            return weather(args[1], OWM_APPID)
-        else:
-            return weather(None, OWM_APPID)
-    return None
 
-def weather(location=None, OWM_APPID):
-    print('location', location)
-    weather_url = 'http://api.openweathermap.org/data/2.5/weather?zip={},us'
-
-    if not location:
-        location = DEFAULT_ZIPCODE 
-    elif location in ZIPCODE:
-        location = ZIPCODE[location]
-    else:
-        location = DEFAULT_ZIPCODE
-
-    weather_url = weather_url.format(location)
-    
-    parm  = {
-        'appid': OWM_APPID,
-        'units': 'imperial',
-    }
-
-    resp = requests.get(weather_url, params=parm)
-    resp = json.loads(resp.text)
-    temp = resp['main']['temp']
-
-    try:
-        desc = resp['weather'][0]['description']
-    except:
-        desc = None
-
-    message = 'Current weather in {} is {}°F, {}'.format(resp['name'], temp, desc)
-    return message
-'''
 if __name__ == '__main__':
     app.run(host='167.172.204.173')
