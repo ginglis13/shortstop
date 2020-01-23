0cae902f0714bb4392ae1cd76beac6e9e9e96fc1
bburger11 <bjburger11@yahoo.com>
2020-01-23 15:00:04 -0800

    initial code for train, need to RTFM

---
+++ modules/train.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+
+def train_handler(message):
+    usage = "usage: !train <trainsystem> <station>\n\
+            \t<trainsystem> Caltrain: CT, Bart: BA\n\
+            \t<station> Stop code for station"
+    trainsystems = ['CT', 'BA']
+
+    message = message.strip().split()[1:]
+
+    if len(message) != 2:
+        return usage
+
+    with open("../.secret", 'r') as f:
+        secrets = json.loads(f.read())
+        token = secrets["train_token"]
+    
+    agency = message[0]
+    stopCode = message[1]
+
+    URL = "http://api.511.org/transit/stoptimetable?api_key={}&OperatorRef={}&MonitoringRef=13008".format(token, agency, stopCode)
+
+    print(URL)
+
+    # r = requests.get(http://api.511.org/transit/StopMonitoring?api_key={your-key}&agency=)
+    
+    return None
+
+if __name__ == '__main__':
+    train_handler("!train BA 23")
+
