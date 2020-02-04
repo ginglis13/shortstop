5651756fef8fa49966bbdeed1125e30b275d2b5e
Brennen <bhogan1@nd.edu>
2020-02-03 19:59:09 -0800

    added random feature

--- modules/tethics_dare.py
+++ modules/tethics_dare.py
@@ -3,26 +3,31 @@ import json
 import random
 
 def dare_handler(sender, message, bot_id, app_id):
-    """Detain a user"""
+    '''Dare a person in the class or randomly dare'''
     message = message.strip().split()
     if len(message) is 2:
         dare = tehics_dares()
-        #dare = 'drink piss'
+        if message[1].lower() == "random":
+            message[1] = random_classmate()
         return "{} has been dared by {} to:\n{}\nTime to earn those participation points".format(message[1], sender, dare)
     else:
         return "Issue a tethics dare.\n usage: !dare <user>\n"
 
+def random_classmate():
+    roster = []
+    with open('tethics_roster.txt') as f:
+        for line in f:
+            roster.append(line.strip())
+    return roster[random.randrange(len(roster))]
 
 def tehics_dares():
-	dares = []
-	#with open('/Users/brennen/School/Junior Year/shortstoploc/dares.txt') as f:
+    dares = []
     with open('dares.txt') as f:
-		for line in f:
-			dares.append(line.strip())
-	return dares[random.randrange(len(dares))]
-		
+        for line in f:
+            dares.append(line.strip())
+    return dares[random.randrange(len(dares))]
+        
 
 
 if __name__ == '__main__':
-    print('Test dare')
-    print(dare_handler('Brennen Hogan','!tehics_dare Dierre',' ',' '))
+    print(dare_handler('Brennen Hogan','!tehics_dare Random',' ',' '))
