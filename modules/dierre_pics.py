3d5dd948d3c1ac8d2b3b508b73759e2d5ab36590
ginglis13 <ginglis05@gmail.com>
2020-03-26 00:54:17 +0000

    image services: change all URLs to be groupme hosted, remove functionality to upload to groupme site from now on, new images will be added to these files only after being uploaded to groupme, so that the upload doesn't happen on each call to the bot

--- modules/dierre_pics.py
+++ modules/dierre_pics.py
@@ -1,30 +1,7 @@
-import requests
-import json
-import pprint
 import random
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
-import os
 
-
-def upload_image_to_groupme(imgURL, app_id):
-	imgRequest = requests.get(imgURL, stream=True)
-	filename = 'temp.png'
-	postImage = None
-	if imgRequest.status_code == 200:
-		# Save Image
-		with open(filename, 'wb') as image:
-			for chunk in imgRequest:
-				image.write(chunk)
-		# Send Image
-		headers = {'content-type': 'application/json'}
-		url = 'https://image.groupme.com/pictures'
-		files = {'file': open(filename, 'rb')}
-		payload = {'access_token': app_id}
-		r = requests.post(url, files=files, params=payload)
-		imageurl = r.json()['payload']['url']
-		os.remove(filename)
-		return imageurl
 
 def dierre_pic_handler(sender, message, bot_id, app_id):
     with open('dierre_pics.txt') as f:
@@ -33,6 +10,7 @@ def dierre_pic_handler(sender, message, bot_id, app_id):
             images.append(line.strip())
         reply_image(images[random.randrange(len(images))], bot_id, app_id)
     return None
+
 
 def dierre_quote():
 	quotes = []
@@ -44,13 +22,10 @@ def dierre_quote():
 
 def reply_image(imgURL, bot_id, app_id):
     url = 'https://api.groupme.com/v3/bots/post'
-    imgURL = upload_image_to_groupme(imgURL, app_id)
-    #urlOnGroupMeService = upload_image_to_groupme(imgURL)
     data = {
 	    'bot_id': bot_id,
 	    'text': dierre_quote(),
 	    'picture_url' : imgURL
 	}
-    # 'picture_url': imgURL
     request = Request(url, urlencode(data).encode())
     json = urlopen(request).read().decode()
--- modules/penny.py
+++ modules/penny.py
@@ -1,30 +1,7 @@
-import requests
-import json
-import pprint
 import random
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
-import os
 
-
-def upload_image_to_groupme(imgURL, app_id):
-	imgRequest = requests.get(imgURL, stream=True)
-	filename = 'temp.png'
-	postImage = None
-	if imgRequest.status_code == 200:
-		# Save Image
-		with open(filename, 'wb') as image:
-			for chunk in imgRequest:
-				image.write(chunk)
-		# Send Image
-		headers = {'content-type': 'application/json'}
-		url = 'https://image.groupme.com/pictures'
-		files = {'file': open(filename, 'rb')}
-		payload = {'access_token': app_id}
-		r = requests.post(url, files=files, params=payload)
-		imageurl = r.json()['payload']['url']
-		os.remove(filename)
-		return imageurl
 
 def penny_pic_handler(sender, message, bot_id, app_id):
     with open('penny.txt') as f:
@@ -37,13 +14,10 @@ def penny_pic_handler(sender, message, bot_id, app_id):
 
 def reply_image(imgURL, bot_id, app_id):
     url = 'https://api.groupme.com/v3/bots/post'
-    imgURL = upload_image_to_groupme(imgURL, app_id)
-    #urlOnGroupMeService = upload_image_to_groupme(imgURL)
     data = {
 	    'bot_id': bot_id,
 	    'text': "dank",
 	    'picture_url' : imgURL
 	}
-    # 'picture_url': imgURL
     request = Request(url, urlencode(data).encode())
     json = urlopen(request).read().decode()
